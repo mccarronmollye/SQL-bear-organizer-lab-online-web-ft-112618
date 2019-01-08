7c16f493162f7719fd661ed56e245cd06c16b6ee
Molly McCarron <mccarronmollye@gmail.com>
2019-01-08 19:02:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Female", "black", "happy", "true");
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Female", "black", "happy", "true");
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3 , "Female", "black", "happy", "true");
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Female", "black", "happy", "true");
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Female", "black", "happy", "true");
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Female", "black", "happy", "true");
